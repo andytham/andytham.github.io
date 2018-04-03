256767eab09fe475acec0e829f46f7b6c2050d28
andy tham <thamandyy@gmail.com>
2018-04-03 14:08:49 -0400

    featured url append fix

--- src/FeaturedProject.jsx
+++ src/FeaturedProject.jsx
@@ -7,7 +7,7 @@ class Project extends Component {
     return (
       <div className="project project-featured featured-big" key={project.key}>
         <div className="project-name"> {project.name} </div>
-        <a className="anchor-image" target="_blank" href={`https://${project.url}`}>
+        <a className="anchor-image" target="_blank" href={`${project.url}`}>
           <img className="project-image featured-start" src={require(`./images/${project.shortname}-1-featured.png`)} alt="fuck off react warning" />
           <img className="project-image featured-start featured-end" src={require(`./images/${project.shortname}-2-featured.png`)} alt="fuck off react warning" />
         </a>
