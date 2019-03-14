b0d5b064c1b28b4177a9a2c12941fc3d5ed8ca55
andy <thamandyy@gmail.com>
2019-03-14 16:44:15 -0400

    Added demo link, adjusted CSS

--- src/Birds.jsx
+++ src/Birds.jsx
@@ -11,24 +11,30 @@ class Birds extends Component {
         <Header />
         <img className="hr" src={hr} />
 				<div className="birds">
-				A project created for a Human Computer Interaction course at Stony Brook University. Developed in Adobe Director and written in Lingo.
-			<br/>
-
-				As it requires Adobe Shockwave or running an exe locally, I have created a video of it in action.
-
-				The "Sing" part of the project no longer works as the license (provided by my professor) for "asPitchDetect Xtra" seems to be invalid now. This plugin handled pitch tracking that allowed users to sing, and the software to capture the pitch, allowing us to place notes on the respective location. A shame as it was an impressive part of the project.
-				
-			<br/>
-				Inspired by this video:
-				
-			<br/>
-				<iframe width="420" height="315"
-					src="https://www.youtube.com/embed/LoM4ZZJ2UrM ">
-				</iframe> 
-				
-			<br/>
+					<span className="birds-header">Harmony of Birds</span>
+					<br/>
+					<br/>
+					A project created for a Human Computer Interaction course at Stony Brook University. Developed in Adobe Director and written in Lingo.
+					<br/>
+					<br/>
+					As it requires Adobe Shockwave or running an exe locally, I have created a video of it in action.
+					<br/>
+					<iframe width="512" height="384"
+						src="https://www.youtube.com/embed/ouMQ5YsdPxc ">
+					</iframe> 
+					<br/>
+					The "Sing" part of the project no longer works as the license (provided by my professor) for "asPitchDetect Xtra" seems to be invalid now. This plugin handled pitch tracking that allowed users to sing, capture the pitch, and allowed me to place notes on the correct location. A shame as it was an impressive part of the project.
+					<br/>
+					<br/>
+					Inspired by this video:
+					
+					<br/>
+					<iframe width="420" height="315"
+						src="https://www.youtube.com/embed/LoM4ZZJ2UrM ">
+					</iframe> 
+					
+					<br/>
 				</div>
-
       </div>
     );
   }
