41bbe2495d13e666a71e4a7d69ff61d2417313a8
andy <thamandyy@gmail.com>
2019-03-14 17:43:38 -0400

    Decided agaisnt forcing open in a new tab

--- src/Project.jsx
+++ src/Project.jsx
@@ -8,7 +8,7 @@ class Project extends Component {
       return (
         <div className="project featured-small" key={project.display_id}>
           <div className="project-name"> {project.name} </div>
-          <a className="anchor-image" target="_blank" href={`${project.url}`}>
+          <a className="anchor-image" href={`${project.url}`}>
             <img className="project-image image-start" src={require(`./images/${project.shortname}-1.png`)} alt="" />
             <img className="project-image hover" src={require(`./images/${project.shortname}-2.png`)} alt="" />
           </a>
@@ -22,10 +22,12 @@ class Project extends Component {
       return (
         <div className="project" key={project.display_id}>
           <div className="project-name"> {project.name} </div>
-          <a target="_blank" href={`${project.url}`}>
+          <a href={`${project.url}`}>
             <img className="project-image image-start" src={require(`./images/${project.shortname}-1.png`)} alt="" />
             <img className="project-image hover" src={require(`./images/${project.shortname}-2.png`)} alt="" />
           </a>
+        
+
 
           <div className="project-tagline hover-text">{project.tagline}</div>
           <div className="project-description hover-text"> {project.description}</div>
