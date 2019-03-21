e84060c9ed42a736ba54d7488e021f854a903e07
andy <thamandyy@gmail.com>
2019-03-21 00:48:47 -0400

    Added lounge details, removed force open new window

--- src/FeaturedProject.jsx
+++ src/FeaturedProject.jsx
@@ -7,7 +7,7 @@ class Project extends Component {
     return (
       <div className="project project-featured featured-big">
         <div className="project-name"> {project.name} </div>
-        <a className="anchor-image" target="_blank" href={`${project.url}`}>
+        <a className="anchor-image" href={`${project.url}`}>
           <img className="project-image featured-start" src={require(`./images/${project.shortname}-1-featured.png`)} alt="Featured" />
           <img className="project-image featured-start featured-end" src={require(`./images/${project.shortname}-2-featured.png`)} alt="Featured 2" />
         </a>
