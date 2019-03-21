0ad9f6de6fbed97a801330e0ba6f8289e02be75d
andy <thamandyy@gmail.com>
2019-03-21 01:24:58 -0400

    Now featuring GIFs

--- src/FeaturedProject.jsx
+++ src/FeaturedProject.jsx
@@ -4,12 +4,13 @@ class Project extends Component {
 // ${project.shortname}
   render() {
     let project = this.props.project;
+    let ext = project.gif ? project.gif : {one: "png", two: "png", oneF: "png", twoF: "png"};
     return (
       <div className="project project-featured featured-big">
         <div className="project-name"> {project.name} </div>
         <a className="anchor-image" href={`${project.url}`}>
-          <img className="project-image featured-start" src={require(`./images/${project.shortname}-1-featured.png`)} alt="Featured" />
-          <img className="project-image featured-start featured-end" src={require(`./images/${project.shortname}-2-featured.png`)} alt="Featured 2" />
+          <img className="project-image featured-start" src={require(`./images/${project.shortname}-1-featured.${ext.oneF}`)} alt="Featured" />
+          <img className="project-image featured-start featured-end" src={require(`./images/${project.shortname}-2-featured.${ext.twoF}`)} alt="Featured 2" />
         </a>
         <div className="text-wrapper">
           <div className="project-tagline hover-text featured-text">{project.tagline}</div>
--- src/Project.jsx
+++ src/Project.jsx
@@ -4,18 +4,19 @@ class Project extends Component {
 // ${project.shortname}
   render() {
     let project = this.props.project;
+    let ext = project.gif ? project.gif : {one: "png", two: "png", oneF: "png", twoF: "png"};
     if (project.featured && !project.hidden){
       return (
         <div className="project featured-small">
           <div className="project-name"> {project.name} </div>
           <a className="anchor-image" href={`${project.url}`}>
-            <img className="project-image image-start" src={require(`./images/${project.shortname}-1.png`)} alt="" />
-            <img className="project-image hover" src={require(`./images/${project.shortname}-2.png`)} alt="" />
+            <img className="project-image image-start" src={require(`./images/${project.shortname}-1.${ext.one}`)} alt="" />
+            <img className="project-image hover" src={require(`./images/${project.shortname}-2.${ext.two}`)} alt="" />
           </a>
 
           <div className="project-tagline hover-text">{project.tagline}</div>
           <div className="project-description hover-text"> {project.description}</div>
-                  <div className="project-technologies hover-text">{project.techonologies}</div>
+          <div className="project-technologies hover-text">{project.techonologies}</div>
         </div>
       );
     } else if (!project.hidden){
@@ -23,12 +24,10 @@ class Project extends Component {
         <div className="project">
           <div className="project-name"> {project.name} </div>
           <a href={`${project.url}`}>
-            <img className="project-image image-start" src={require(`./images/${project.shortname}-1.png`)} alt="" />
-            <img className="project-image hover" src={require(`./images/${project.shortname}-2.png`)} alt="" />
+            <img className="project-image image-start" src={require(`./images/${project.shortname}-1.${ext.one}`)} alt="" />
+            <img className="project-image hover" src={require(`./images/${project.shortname}-2.${ext.two}`)} alt="" />
           </a>
         
-
-
           <div className="project-tagline hover-text">{project.tagline}</div>
           <div className="project-description hover-text"> {project.description}</div>
           <div className="project-technologies hover-text">{project.techonologies}</div>
