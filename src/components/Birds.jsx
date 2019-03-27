428eeca62e94c6487778fcdef3ba0bc45fb624df
andy <thamandyy@gmail.com>
2019-03-26 23:09:11 -0400

    Birds CSS

--- src/components/Birds.jsx
+++ src/components/Birds.jsx
@@ -9,11 +9,10 @@ class Birds extends Component {
 				<div className="birds">
 					<span className="birds-header">Harmony of Birds</span>
 					<br/>
-					<br/>
 					A project created for a Human Computer Interaction course at Stony Brook University. Developed in Adobe Director and written in Lingo.
 					<br/>
 					<br/>
-					As it requires Adobe Shockwave or running an exe locally, I have created a video of it in action.
+					As it requires Adobe Shockwave or running an exe locally, I have created a video of it in action:
 					<br/>
 					<iframe title="Harmony of Birds" className="birds-video" width="512" height="384"
 						src="https://www.youtube.com/embed/ouMQ5YsdPxc" frameBorder="0" allowFullScreen>
@@ -22,8 +21,8 @@ class Birds extends Component {
 					The "Sing" part of the project no longer works as the license (provided by my professor) for "asPitchDetect Xtra" seems to be invalid now. This plugin handled pitch tracking that allowed users to sing, capture the pitch, and allowed me to place notes on the correct location. A shame as it was an impressive part of the project.
 					<br/>
 					<br/>
-					Inspired by this video:
-					
+					<br/>
+					<span className="inspiration-header">Inspired by this video:</span>
 					<br/>
 					<iframe title="inspiration" className="inspiration-video" width="448" height="252" src="https://www.youtube.com/embed/LoM4ZZJ2UrM" frameBorder="0" allowFullScreen></iframe>
 					
